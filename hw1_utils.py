b63446fe68ef3b9d894be36c98201bb19c77622c
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2023-01-25 02:07:33 +0000

    Added "myloss" function

--- hw1_utils.py
+++ hw1_utils.py
@@ -32,20 +32,6 @@ def xent(X, Y, theta):
 
     b = Y.shape[0]  # batch size
 
-    e3 = np.hstack((np.zeros((theta.shape[0], theta.shape[1]//2)), np.ones((theta.shape[0], theta.shape[1] - theta.shape[1]//2))))
-
-    # print(-np.diagonal(np.matmul(Y, np.matmul(X, e3.T).T)))
-    # print(np.log(
-    #     np.sum(np.exp(np.matmul(X, e3.T)), axis=1)))
-    
-    # print(-np.diagonal(np.matmul(Y, np.matmul(X, e3.T).T)) + np.log(
-    #     np.sum(np.exp(np.matmul(X, e3.T)), axis=1)))
-    
-    # print(np.diagonal(np.matmul(Y, np.matmul(X, theta.T).T)) + np.log(
-    #     np.sum(np.exp(np.matmul(X, theta.T)), axis=1)))
-
-    # print(np.matmul(Y.T, np.matmul(X, theta.T)))
-
     return 1 / b * (np.sum(-np.diagonal(np.matmul(Y, np.matmul(X, theta.T).T)) + np.log(
         np.sum(np.exp(np.matmul(X, theta.T)), axis=1)), axis=0, keepdims=True))
 
@@ -59,21 +45,9 @@ def grad_theta_xent(X, Y, theta):
         K x D_1 gradient of xent(X, Y, theta).sum() wrt theta
     """
     s = np.matmul(X, theta.T)
-    grad_s = X
 
-    b = Y.shape[0]  # batch size
+    return 1 * (np.matmul((-Y + softmax(s)).T, X))
 
-    # print(Y.shape, X.shape, theta.shape)
-    # print((-np.matmul(Y.T, grad_s)).shape)
-    # print(np.diagonal(np.matmul(np.exp(s).T, grad_s)).shape)
-    # print(np.sum(np.exp(s), axis=1, keepdims=True).shape)
-
-    # return 1 / b * (np.sum(-np.matmul(Y.T, grad_s) + np.divide(np.matmul(np.exp(s).T, grad_s)), np.sum(
-    #     np.exp(s), axis=1, keepdims=True)), axis=0, keepdims=True)
-
-    # print(np.sum(-np.matmul(Y.T, grad_s),axis=0, keepdims=True))
-
-    return 1 / b * (-np.matmul(Y.T, grad_s) + np.sum(X, axis = 0, keepdims=True))
 
 
 def mse(X, Y, theta):
@@ -86,8 +60,11 @@ def mse(X, Y, theta):
     """
 
     delta = Y - np.matmul(X, theta.T)
+    b = Y.shape[0]
+    k = Y.shape[1]
 
-    return 1 / Y.shape[1] * np.sum(np.diagonal(np.matmul(delta.T, delta)), keepdims=True)
+    return 1 / b / k * np.sum(np.diagonal(np.matmul(delta.T, delta)))
+    
 
 
 def grad_theta_mse(X, Y, theta):
@@ -100,8 +77,10 @@ def grad_theta_mse(X, Y, theta):
     """
 
     b = Y.shape[0]  # batch size
+    k = Y.shape[1]
+    s = np.matmul(X, theta.T)
 
-    return 1 / b * np.sum(2 / Y.shape[1] * np.matmul((np.matmul(X, theta.T) - Y).T, X), axis=0, keepdims=True)
+    return -2 / k * np.matmul((Y - s).T, X)
 
 
 def softmse(X, Y, theta):
@@ -124,11 +103,11 @@ def grad_theta_softmse(X, Y, theta):
     returns:
         K x D_1 gradient of softmse(X, Y, theta).sum() wrt theta
     """
-    # return 2 / Y/shape[1] * (Y - softmax(np.matmul(X, theta.T))), (np.matmul(softmax(np.matmul(X, theta.T)).T, X) - np.matmul(softmax(np.matmul(X, theta.T)).T, softmax(np.matmul(X, theta.T))))
 
-    b = Y.shape[0]
+    k = Y.shape[1]
+    s = np.matmul(X, theta.T)
 
-    return 1 / b * (-np.matmul(Y.T, softmax(X)) + np.sum(softmax(X), axis = 0, keepdims=True))
+    return - 2 / k * np.matmul((np.multiply(Y-softmax(s), softmax(s)) - np.multiply(Y-softmax(s), np.multiply(softmax(s), softmax(s)))).T, X)
 
 
 def myloss(X, Y, theta):
@@ -139,7 +118,13 @@ def myloss(X, Y, theta):
     returns:
        bsz-length array of losses
     """
-    finiteDifferences(X,Y, theta)
+    # finiteDifferences(X,Y, theta)
+
+    k = Y.shape[1]
+    s = np.matmul(X, theta.T)
+    sigmoid = np.divide(np.ones(s.shape), np.ones(s.shape) + np.exp(-s))
+
+    return 1 / k * np.sum(np.diagonal(np.matmul((Y - sigmoid).T, (Y - sigmoid))), keepdims= True)
 
 
 def grad_theta_myloss(X, Y, theta):
@@ -150,7 +135,11 @@ def grad_theta_myloss(X, Y, theta):
     returns:
         K x D_1 gradient of myloss(X, Y, theta).sum() wrt theta
     """
-    # TODO: your code here
+    k = Y.shape[1]
+    s = np.matmul(X, theta.T)
+    sigmoid = np.divide(np.ones(s.shape), np.ones(s.shape) + np.exp(-s))
+
+    return - 2 / k * np.matmul(((Y - sigmoid) - np.multiply(Y - sigmoid, sigmoid)).T, X)
 
 
 def softmax(X):
@@ -175,17 +164,3 @@ def finiteDifferences(X, Y, theta):
     print(grad_theta_softmse(X, Y, theta)[0, 0])
     print((softmse(X, Y, theta + e2 * constant) - softmse(X, Y, theta - e2 * constant)) / (2 * constant))
     # print(mse(X, Y, theta))
-    # print(softmse(X, Y, theta))
-    # print(grad_theta_mse(X, Y, theta)[0, 0])
-    # print(mse(X, Y, e2))
-    # print(mse(X, Y, np.matmul(e2, e * (1 + constant))) - mse(X, Y, np.matmul(e2, e * (1 - constant))))
-    # print(theta)
-    # print(grad_theta_mse(X, Y, theta))
-    # print(theta + e3 * constant)
-    # epsilon = 1 / 10 ** 5
-    # # print(grad_theta_xent(X, Y, theta))
-    # print(xent(X, Y, e3 * .233))
-    # print(xent(X, Y, theta + e3 * constant))
-    # print((1+ epsilon) * e)
-    # print(np.matmul(theta, (1 + epsilon) * e).shape)
-    # print((xent(X, Y, np.matmul(theta, (1 + epsilon) * e))))
